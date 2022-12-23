a513c5fe59242ad4a7a0b314b06062e3f6341092
lewisc99 <lewiscontato99@gmail.com>
2022-12-23 19:34:12 -0300

    added mongoRepository using inversion of control

--- Ms.Person/Ms.Person/Entities/PersonEntity.cs
+++ Ms.Person/Ms.Person/Entities/PersonEntity.cs
@@ -1,10 +1,11 @@
 ﻿
+using Ms.Common;
+
 namespace Ms.Person.Entities
 {
-    public class Person
+    public class PersonEntity:IEntity
     {
         public string Name { get; set; }
-
         public string Doc { get; set; }
     }
 }
--- Ms.Person/Ms.Person/Startup.cs
+++ Ms.Person/Ms.Person/Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Ms.Common.MongoDB;
+using Ms.Common.Settings;
+using Ms.Person.Entities;
 
 namespace Ms.Person
 {
@@ -14,18 +17,26 @@ namespace Ms.Person
             Configuration = configuration;
         }
 
+        private ServiceSettings serviceSettings;
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
+
+            serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+            services.AddMongo()
+                .addMongoRepository<PersonEntity>("persons");
+              
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ms.Person", Version = "v1" });
             });
-
 
 
         }
